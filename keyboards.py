df11af3684feb17b61a014209de36a7768b693a0
Larcev <rustamtastemiroff@yandex.ru>
2025-05-30 02:21:49 +0600

    Update keyboards.py
    Удалил ненужный код

--- keyboards.py
+++ keyboards.py
@@ -1,19 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-'''
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='catalog'),
-    KeyboardButton(text='basket')],
-    [KeyboardButton(text='contacts')],
-    [KeyboardButton(text='location', request_location=True),
-    KeyboardButton(text='Отправить контакт', request_contact=True)],
-
-],
-    resize_keyboard=True,
-    input_field_placholder='Выберите пункт ниже'
-)
-'''
 #тут кнопки, какие есть
 main_kb = ReplyKeyboardMarkup(
     keyboard=[
@@ -23,32 +10,8 @@ main_kb = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
-'''
-inline_main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Досрочное погашение', callback_data='fast'),
-     InlineKeyboardButton(text='Обычное погашение', callback_data='normal')],
-    [InlineKeyboardButton(text='52 недели богатства', callback_data='fifty_two')]
-])
-
-'''
 
 #кнопка назад
 back = InlineKeyboardMarkup(inline_keyboard = [
     [InlineKeyboardButton(text="назад", )],
 ])
-
-
-
-
-
-
-'''
-async def catalog_builder():
-    brands = ["Nike", "Adidas", "Puma", "Reebok", "New Balance",
-              "Under Armour", "Vans", "Converse", "Fila", "Asics", "Skechers"]
-    Keyboard = InlineKeyboardBuilder()
-    for brand in brands:
-        Keyboard.add(InlineKeyboardButton(text=brand, callback_data=f'item_{brand}'))
-
-    return Keyboard.adjust(2).as_markup()
-'''
